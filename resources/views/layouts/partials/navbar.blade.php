6e1462a2ed08a1fde47ce568b50f3afa9ec283c9
Tulio <tquintanaa@miumg.edu.gt>
2026-09-09 18:09:56 -0600

    fix(views): arreglar boton de Logout en navbar
    El link de Logout era href=# (dejaba un TODO explicito sin
    resolver) - no ejecutaba ninguna accion. Cambiado a un form POST
    real con csrf y route(logout), igual que Breeze requiere para
    esta ruta. Probado: logout funciona y redirige a login.

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -17,8 +17,13 @@
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                 <li><a class="dropdown-item" href="#!">Settings</a></li>
                 <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                <!-- TODO: cambiar # por route('logout') y hacer que funcione el logout -->
-                <li><a class="dropdown-item" href="#">Logout</a></li>
+                <li>
+                    {{-- FIX I1: logout ahora es un form POST real (antes href="#" sin funcionar) --}}
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="dropdown-item">Logout</button>
+                    </form>
+                </li>
             </ul>
         </li>
     </ul>
